9c85961dcc11e4645239043d4edb7bb6190fcd77
Sidney <sidney_carlini@hotmail.com>
2021-09-21 18:00:20 -0300

    inicio do desenvolvimento da logica dos botoes de add e remove points

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-console.log("teste");
+
+
+// function add(){
+//     var team1Points = document.querySelector("#team1Points");
+
+//     team1Points.value = team1Points.textContent++
+
+
+function add(a){
+
+    dad = a.parentNode;
+    grandpa = dad.parentNode;
+    sibling = grandpa.parentNode;
+    targetElement = sibling.previousElementSibling;
+    h1Target = targetElement.children;
+
+    h1Target[0].value = h1Target[0].textContent++
+
+console.log(h1Target[0].textContent);
+
+}
+
