63345e3d3c14817c04416b7f61e8c4f6051f4c20
Sidney <sidney_carlini@hotmail.com>
2021-09-24 01:30:07 -0300

    Logica do botao truco!

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
+var clicks = 0;
+
+function truco(){
+    let trucoBox = document.getElementById("trucoButtonText");
+
+    clicks++
+    switch(clicks){
+        case 1:
+            trucoBox.innerHTML = "É 6 CUZAO!";
+            break;
+        
+        case 2:
+            trucoBox.innerHTML = "9 ENTAO!";
+            break;
+        
+        case 3:
+            trucoBox.innerHTML = "12 PRA ACABAR LOGO!";
+            break;
+
+        default:
+            trucoBox.innerHTML = "É truco papudo!"
+            clicks = 0;
+            break;
+    }
+    
+
+
+
+    console.log(trucoBox,clicks);
+
+
+}
+
 function getTarget(target){
     //Escala os niveis de hierarquia
     //se posicionando no elemento a ser alterado
@@ -21,5 +54,11 @@ function remove(element){
 
     target = getTarget(element);
 
-    target.value = target.textContent--;
+    if(target.textContent > 0){
+        target.value = target.textContent--;
+    }
 }
+
+
+//validar se < 0 
+//validar se =11
